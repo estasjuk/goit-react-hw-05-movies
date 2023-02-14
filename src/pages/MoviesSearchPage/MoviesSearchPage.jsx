1ef4002c8f169cb6bb1bb5bdabffdaab07717da9
Olena Stasiuk <estasjuk@gmail.com>
2023-02-14 15:59:38 +0200

    add singleMoviePage

--- src/pages/MoviesSearchPage/MoviesSearchPage.jsx
+++ src/pages/MoviesSearchPage/MoviesSearchPage.jsx
@@ -3,7 +3,6 @@ import Searchbar from '../../components/MoviesSearch/Searchbar/Searchbar';
 const MoviesSearchPage = () => {
   return (
     <>
-      <h1>Movies</h1>
       <Searchbar />
     </>
   );
--- src/pages/SingleMoviePage/SingleMoviePage.jsx
+++ src/pages/SingleMoviePage/SingleMoviePage.jsx
@@ -1,40 +1,7 @@
-import { useState, useEffect } from 'react';
-import { useParams } from 'react-router-dom';
-import { getMovieDetails } from 'shared/services/movies-api';
+import SingleMovie from 'components/MoviesSearch/SingleMovie/SingleMovie';
 
 const SingleMoviePage = () => {
-  const [movie, setMovie] = useState(null);
-  const [loading, setLoading] = useState(false);
-  const [error, setError] = useState(null);
-
-  const { movieId } = useParams();
-
-  useEffect(() => {
-    const fetchMovieDetails = async () => {
-      try {
-        setLoading(true);
-        const data = await getMovieDetails(movieId);
-        setMovie(data);
-      } catch ({ response }) {
-        setError(response.data.message);
-      } finally {
-        setLoading(false);
-      }
-    };
-    fetchMovieDetails();
-  }, [movieId]);
-
-  console.log(movie);
-
-  return (
-    <div>
-      <h1>
-        {movie?.title}
-        {movie?.release_date}
-      </h1>
-      <p>{movie?.overview}</p>
-    </div>
-  );
+  return <SingleMovie />;
 };
 
 export default SingleMoviePage;
